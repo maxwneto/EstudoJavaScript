0e47bb982735a89cf2bf83bd0f742fbb4865a63a
maxwneto <maxwneto@gmail.com>
2020-02-09 18:29:08 -0300

    estudo while e Do/while

---
+++ aula16/ambiente.js
@@ -0,0 +1,5 @@
+var saudacao = 1
+ while (saudacao <= 10){
+     console.log(`${saudacao} - Tudo bem?`)
+     saudacao ++
+ }
---
+++ aula16/ambiente2.js
@@ -0,0 +1,5 @@
+var saudacao = 1
+do{
+    console.log(`Passo ${saudacao}!`)
+    saudacao++
+}while (saudacao <= 100)
